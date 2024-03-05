56f2021f664420786578d5a815d667ef8daf2320
CelsonF <celsondev@outlook.com>
2024-03-05 00:57:29 -0300

    Feat:Create project managment jobs

---
+++ src/test/java/br/com/celsonfernando/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.celsonfernando.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
