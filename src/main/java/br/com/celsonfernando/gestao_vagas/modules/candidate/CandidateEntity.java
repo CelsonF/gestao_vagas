c80471691d24bbe0c70c5ff303408605ebea04ec
CelsonF <celsondev@outlook.com>
2024-04-17 22:23:57 -0300

    feat: Implementate useCase

--- src/main/java/br/com/celsonfernando/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/celsonfernando/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
