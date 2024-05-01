376844a1f5b929aadca0118f165c5341da8b262a
CelsonF <celsondev@outlook.com>
2024-05-01 18:53:12 -0300

    feat: Add JWT and security layer.

--- src/main/java/br/com/celsonfernando/gestao_vagas/modules/company/useCase/AuthCompanyUseCase.java
+++ src/main/java/br/com/celsonfernando/gestao_vagas/modules/company/useCase/AuthCompanyUseCase.java
@@ -3,7 +3,12 @@ package br.com.celsonfernando.gestao_vagas.modules.company.useCase;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,6 +17,9 @@ import br.com.celsonfernando.gestao_vagas.modules.company.repositories.CompanyRe
 
 @Service
 public class AuthCompanyUseCase {
+    
+    @Value("${security.token.secret}")
+    private String secretKey;
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -19,7 +27,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
                     throw new UsernameNotFoundException("Company not found.");
@@ -31,5 +39,11 @@ public class AuthCompanyUseCase {
             throw new AuthenticationException();
         }
         
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+
+        return token;
     }
 }
