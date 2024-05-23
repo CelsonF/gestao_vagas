072e005c599c6e9964b133a21b3eff8aa5fbfb37
CelsonF <celsondev@outlook.com>
2024-05-23 20:22:49 -0300

    feat: create Pre Authorize.

--- src/main/java/br/com/celsonfernando/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/celsonfernando/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,8 +1,11 @@
 package br.com.celsonfernando.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -26,7 +29,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
             FilterChain filterChain)
             throws ServletException, IOException {
 
-        SecurityContextHolder.getContext().setAuthentication(null);
+        //SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if (request.getRequestURI().startsWith("/candidate")) {
@@ -39,8 +42,19 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                 }
 
                 request.setAttribute("candidate_id", token.getSubject());
-                System.out.println(" =========== TOKEN ============ ");
-                System.out.println(token);
+
+                var roles = token.getClaim("roles").asList(Object.class);
+                var grants = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
+                .toList();
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        token.getSubject(),
+                        null,
+                        grants);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
             }
         }
 
