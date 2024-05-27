8dc41c4a0867ab177bdf9920dd67421e9e044ae0
CelsonF <celsondev@outlook.com>
2024-05-26 21:27:49 -0300

    feat: Add Swagger definitions in project.

--- src/main/java/br/com/celsonfernando/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/celsonfernando/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.celsonfernando.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.celsonfernando.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.celsonfernando.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.celsonfernando.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.celsonfernando.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.celsonfernando.gestao_vagas.modules.company.useCase.ListAllJobsByFilterUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
-    
+
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
@@ -34,9 +50,9 @@ public class CandidateController {
 
         try {
             var profile = this.profileCandidateUseCase
-            .execute(UUID.fromString(idCandidate.toString()));
-            
-            return ResponseEntity.ok().body(profile); 
+                    .execute(UUID.fromString(idCandidate.toString()));
+
+            return ResponseEntity.ok().body(profile);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -46,11 +62,27 @@ public class CandidateController {
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
-            
+
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informaçõoes do Candidato")
+    @Operation(summary = "Listagem de vagas disponível para o candidato",
+    description = "Essa função é responsável por listar todas as vagas disponíveis, baseadas no filtro.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+        })
+    })
+
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+       return this.listAllJobsByFilterUseCase.execute(filter);
+    }
+    
 }
